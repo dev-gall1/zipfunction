2d9673d7c8204aa1d2d8a9a25ced933b24c89470
asg <sg.andre.dados1@gmail.com>
2023-03-28 23:32:10 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import zipfile
+import pathlib
+
+#compressed
+def make_archive(filepaths,destination):
+    path = pathlib.Path(destination,"compressed.zip")
+    with zipfile.ZipFile(path,"w") as file:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            print(filepath)
+            file.write(filepath,arcname=filepath.name)
