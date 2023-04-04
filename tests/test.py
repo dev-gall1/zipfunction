969213ef3e93e19e3f2da0686d45966eb7dc91f9
asg <sg.andre.dados1@gmail.com>
2023-04-04 00:39:29 -0300

    add test

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+import zipfile
+import pathlib
+def make_archive(filepaths, destination):
+    path = pathlib.Path(destination, "compressed.zip")
+    with zipfile.ZipFile(path, "w") as file:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            print(filepath)
+            file.write(filepath, arcname=filepath.name)
