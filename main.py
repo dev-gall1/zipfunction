0bb53d307338648f6c3cf2412bc989b93cf3973c
Andre Galli <123599323+dev-gall1@users.noreply.github.com>
2023-04-04 00:45:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def a(x,y):
-   total = x + y
-    return total
+
+import zipfile
+import pathlib
+
+def make_archive(filepaths, destination):
+    path = pathlib.Path(destination, "compressed.zip")
+    with zipfile.ZipFile(path, "w") as file:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            print(filepath)
+            file.write(filepath, arcname=filepath.name)
